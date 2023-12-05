7076033030f7d0f66a991ebd5d0e1201f187188e
guibarross <guilhermecbarros7@gmail.com>
2023-12-05 13:47:33 -0300

    first commit

---
+++ app/Livewire/ShowTweets.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowTweets extends Component
+{
+    
+public $message = "Teste";
+
+    public function render()
+    {
+        return view('livewire.show-tweets');
+    }
+}
